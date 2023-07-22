779b130e797507c86f046546c1162bb0456061d8
muzaffertuna <toktastuna@gmail.com>
2023-07-22 19:32:20 +0300

    f

--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,28 @@
 /*   By: mtoktas <mtoktas@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 19:59:08 by mtoktas           #+#    #+#             */
-/*   Updated: 2023/07/22 19:03:15 by mtoktas          ###   ########.fr       */
+/*   Updated: 2023/07/22 19:30:30 by mtoktas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 11
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-int ft_strlen(char *s);
-char *ft_strjoin(char *s1, char *s2);
-char *ft_strchr(char *s, int c);
+int		ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_next(char *str);
+char	*ft_line(char *str);
+char	*ft_read(char *str, int fd);
+char	*get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,64 +6,60 @@
 /*   By: mtoktas <mtoktas@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 19:59:02 by mtoktas           #+#    #+#             */
-/*   Updated: 2023/07/22 18:48:25 by mtoktas          ###   ########.fr       */
+/*   Updated: 2023/07/22 19:23:44 by mtoktas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_strlen(char *s)
+int	ft_strlen(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(s[i])
-        i++;
-    return(i);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-char *ft_strjoin(char *str, char *buf)
+char	*ft_strjoin(char *str, char *buf)
 {
-    int i;
-    int j;
-    char *res;
-    
-    i = 0;
-    j = 0;
-    if(!str)
-    {
-        str = (char *)malloc(sizeof(char));
-        str[0] = '\0';
-    }
-    if(!str || !buf)
-        return (NULL);
-    res = (char *)malloc(sizeof(char) * (ft_strlen(str) + ft_strlen(buf) + 1));
-    if(!res)
-        return(NULL);
-    while(str[i])
-    {
-        res[i] = str[i];
-        i++;
-    }
-    while(buf[j])
-    {
-        res[i++] = buf[j++];
-    }
-    res[i] = '\0';
-    free(str);
-    return(res);
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	if (!str)
+	{
+		str = (char *)malloc(sizeof(char));
+		str[0] = '\0';
+	}
+	res = (char *)malloc(sizeof(char) * (ft_strlen(str) + ft_strlen(buf) + 1));
+	if (!res)
+		return (NULL);
+	while (str[i])
+	{
+		res[i] = str[i];
+		i++;
+	}
+	while (buf[j])
+		res[i++] = buf[j++];
+	res[i] = '\0';
+	free(str);
+	return (res);
 }
 
-char *ft_strchr(char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(s[i])
-    {
-        if(s[i] == c)
-            return(s + i);
-        i++;
-    }
-    return(0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (0);
 }
